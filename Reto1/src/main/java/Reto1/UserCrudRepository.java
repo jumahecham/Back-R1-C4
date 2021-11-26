a028114ddf5b17c680fef4b1acac8791eadf8779
jumahecham <juan.montanez.mt@correo.usa.edu.co>
2021-11-26 13:43:42 -0500

    first commit

---
+++ Reto1/src/main/java/Reto1/UserCrudRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto1;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author juanp
+ */
+public interface UserCrudRepository  extends CrudRepository <User, Integer>{
+    Optional <User> findByEmail (String email);
+    Optional <User> findByEmailAndPassword (String email, String password);  
+}
